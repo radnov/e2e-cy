f18e7974a32ba6004d04db5b5fd8aa92d57c4f66
radnov <radnov@pm.me>
2022-07-19 12:14:03 +0300

    revert changes in merge launches script

--- merge_launches.py
+++ merge_launches.py
@@ -1,9 +1,9 @@
 import requests, os, time
 from datetime import datetime
-ENABLED = os.getenv('CYPRESS_REPORT_PORTAL_ENABLED', False)
+ENABLED = os.environ['CYPRESS_REPORT_PORTAL_ENABLED']
 URL = "https://test.tools.dhis2.org/reportportal/api/v1/dhis2_auto"
-REPORT_PORTAL_TOKEN = os.getenv('RP_TOKEN')
-CI_BUILD_ID = os.getenv('CI_BUILD_ID', 'test0002')
+REPORT_PORTAL_TOKEN = os.getenv['RP_TOKEN']
+CI_BUILD_ID = os.getenv['CI_BUILD_ID']
 
 if ENABLED == False:
   exit(0)
